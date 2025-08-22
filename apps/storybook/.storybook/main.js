96e37f8afde8d15d957ad82a309c2bbcdb1a0f85
kecan <1905537268@qq.com>
2025-08-22 17:52:52 +0800

    fix: simplify storybook configuration to resolve dependency conflicts

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -3,24 +3,12 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     '../../../packages/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-  ],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
   docs: {
     autodocs: 'tag',
-  },
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: prop =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
   },
 }
